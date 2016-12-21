cc5be3ab7d246469a84ca94ef0d603e81554ed01
rop2001 <rop2001@gmail.com>
2016-12-21 17:36:47 +0900

    app.js commit

--- nodejsTest/app.js
+++ nodejsTest/app.js
@@ -2,8 +2,30 @@ var firebase = require("firebase");
 var express = require("express");
 var app = express(); 
 var server = require("http").createServer(app);
+var bodyParser = require('body-parser');
+var mysql = require("mysql");
 var io = require("socket.io")(server);
 var port = process.env.PORT || 3000;
+var queryString = require("querystring");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
+
+var connection = mysql.createConnection({
+
+});
+
+connection.connect(function(err){
+	if(err){
+		console.error("mysql connection error");
+		console.error(err);
+		throw err;
+	}else{
+		console.log("database connect");
+	}
+});
+
+console.log("database connection pass");
 
 //로컬에서 돌릴때는 주석 처리
 //firebase.intializeApp({serviceAccount: ""}); 
@@ -27,84 +49,6 @@ io.on("connection", function (socket) {
 	});
 });
 
-
-																																																																																												// listens
-																																																																																												// on
-																																																																																												// port
-																																																																																												// 3000
-																																																																																												// server.listen(port,
-																																																																																												// function
-																																																																																												// () {
-																																																																																												// console.log("Server
-																																																																																												// listening
-																																																																																												// on
-																																																																																												// port
-																																																																																												// %d",
-																																																																																												// port);
-																																																																																												// });
-																																																																																												// //
-																																																																																												// Routing
-																																																																																												// to
-																																																																																												// static
-																																																																																												// files
-																																																																																												// app.use(express.static(__dirname
-																																																																																												// +
-																																																																																												// "/public"));
-																																																																																												// //
-																																																																																												// Socket
-																																																																																												// server
-																																																																																												// listens
-																																																																																												// on
-																																																																																												// connection
-																																																																																												// event
-																																																																																												// io.on("connection",
-																																																																																												// function
-																																																																																												// (socket)
-																																																																																												// {
-																																																																																												// console.log("Connected
-																																																																																												// and
-																																																																																												// ready!");
-																																																																																												// //
-																																																																																												// firebase
-																																																																																												// reference
-																																																																																												// listens
-																																																																																												// on
-																																																																																												// value
-																																																																																												// change,
-																																																																																												// //
-																																																																																												// and
-																																																																																												// return
-																																																																																												// the
-																																																																																												// data
-																																																																																												// snapshot
-																																																																																												// as
-																																																																																												// an
-																																																																																												// object
-																																																																																												// firebaseRef.on("value",
-																																																																																												// function
-																																																																																												// (snapshot)
-																																																																																												// {
-																																																																																												// var
-																																																																																												// colorChange
-																																																																																												// =
-																																																																																												// snapshot.val();
-																																																																																												// //
-																																																																																												// Print
-																																																																																												// the
-																																																																																												// data
-																																																																																												// object's
-																																																																																												// values
-																																																																																												// console.log("snapshot
-																																																																																												// R: "
-																																																																																												// +
-																																																																																												// colorChange.r);
-																																																																																												// console.log("snapshot
-																																																																																												// B: "
-																																																																																												// +
-																																																																																												// colorChange.b);
-																																																																																												// console.log("snapshot
-																																																																																												// G: "
-																																																																																												// +
-																																																																																												// colorChange.g);
-																																																																																												// });
-																																																																																												// });
+app.post("/test.json",function(req,res){
+		console.log(req.body.id);
+});
